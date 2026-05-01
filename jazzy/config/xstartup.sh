d95e4c649162b6b2260271920352dcd741b91ac4
Sylvain Argentieri <sylvain.argentieri@sorbonne-universite.fr>
2026-05-01 21:04:12 +0200

    Update default webots layout + disable screensaver
    I had to patch webots_launcher.py to specify default layout config if not specified.

--- jazzy/config/xstartup.sh
+++ jazzy/config/xstartup.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 unset DBUS_SESSION_BUS_ADDRESS
+# VNC sessions are expected to stay visible and interactive, so disable
+# the X11 screen saver and display power management entirely.
+xset s off
+xset s noblank
+xset -dpms
 mate-session
